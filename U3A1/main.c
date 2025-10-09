13610b334ccf4983a5b01899c7c19f640362b9d8
Luis Mendoza <desarrollo@dxt.com.mx>
2025-10-08 19:52:38 -0600

    u3a1 inicio

---
+++ U3A1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+//Definir funciones void
+
+void fisica();
+void matematicas();
+void geometria();
+
+void menuPrincipal();
+
+int main(){
+	const float PI = 3.1415f;
+	int Acc,subAcc;
+	float distancia,altura,base,radio,velocidad,tiempo;
+	
+	
+	menuPrincipal();
+	
+	while(){
+		
+	}
+	
+	printf("Gracias por utilizar el sistema");
+	return 0;
+}
+
+
+
+void menuPrincipal(){
+	printf("Hello world");
+}
